111fde568a89aaf92abe52604318486c74887ffd
Subhanshu Chaddha <subhanshu.chaddha@devslane.com>
2023-08-25 21:35:58 +0530

    minor

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-  baseURL: 'http://finvestfx-be-6faacdf06e11.herokuapp.com',
+  baseURL: 'https://finvestfx-be-6faacdf06e11.herokuapp.com',
 });
 
 const menuItemsApi = {
